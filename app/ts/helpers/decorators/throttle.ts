6303be7a57392d6a277e65421043630cacbbfafa
Vinicius Crestani <vinicr@gmail.com>
2021-08-07 19:01:42 -0300

    init

---
+++ app/ts/helpers/decorators/throttle.ts
@@ -0,0 +1,14 @@
+export function throttle(millisegundos = 500) {
+
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+
+        const metodoOriginal = descriptor.value;
+        let timer = 0;
+        descriptor.value = function (...args: any[]) {
+            if (event) event.preventDefault();
+            clearInterval(timer)
+            timer = setTimeout(() => metodoOriginal.apply(this, args), millisegundos)
+        }
+        return descriptor;
+    }
+}
---
+++ app/ts/models/index.ts
@@ -0,0 +1,6 @@
+export * from './Imprimivel';
+export * from './Negociacao';
+export * from './Negociacoes';
+export * from './NegociacaoParcial';
+export * from './Igualavel';
+export * from './MeuObjeto';
